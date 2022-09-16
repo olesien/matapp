7d341071aea0420d58020ce74e3186f3c7c51e54
olesien <olesien2@gmail.com>
2022-09-16 15:18:48 +0200

    Init

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,36 @@
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link, NavLink } from "react-router-dom";
+import { NavDropdown } from "react-bootstrap";
+
+const Navigation = () => {
+    return (
+        <Navbar bg="dark" variant="dark" expand="md">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    <img
+                        src="/src/assets/icons/favicon.svg"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />{" "}
+                    Firebase Todos
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto align-items-center">
+                        <Nav.Link as={NavLink} to="/">
+                            Home
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Navigation;
