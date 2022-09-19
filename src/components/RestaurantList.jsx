7dc3ad7a2ea911c2a3210831ad22f2f6bf672bbe
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-09-19 10:37:35 +0200

    Created hooks for fetching collections from firestore. Now displaying data on home page temporarily.

---
+++ src/components/RestaurantList.jsx
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container'
+import useStreamRestaurants from '../hooks/useStreamRestaurants'
+
+const RestaurantList = () => {
+    const restaurants = useStreamRestaurants()
+    console.log(restaurants)
+    return (
+        <Container>
+            <h2>Restaurant list</h2>
+            {restaurants && restaurants.length > 0 && (
+                restaurants.map(restaurant => (
+                    <div key={restaurant.id}>
+                        <h4>{restaurant.name}</h4>
+                        <p>{restaurant.description}</p>
+                    </div>
+                ))
+            )}
+        </Container>
+    )
+}
+
+export default RestaurantList
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Container from "react-bootstrap/Container";
+import RestaurantList from "../components/RestaurantList";
 
 const HomePage = () => {
     return (
         <Container className="py-3">
             <h1>Welcome!</h1>
+            <RestaurantList />
         </Container>
     );
 };
