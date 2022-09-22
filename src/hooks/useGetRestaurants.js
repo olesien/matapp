006500aa98f8dfe3f74078e9b6028bb10fd0f353
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-09-22 14:41:56 +0200

    Both restaurant filters are now working.

--- src/hooks/useGetRestaurants.js
+++ src/hooks/useGetRestaurants.js
@@ -3,14 +3,20 @@ import useGetCollection from "./useGetCollection"
 import { db } from "../firebase"
 
 const useGetRestaurants = (filterOptions) => {
+  // filterOptions is passed along to useGetCollection so that the restaurants are refetched when the filter options change.
+  const queryConstraints = []
   if (filterOptions) {
-    const restaurantsRef = collection(db, 'restaurants')
-    const q = query(restaurantsRef, where("offers", "==", filterOptions.option2))
-    console.log("Filter options in hook!", filterOptions.option2)
-    return useGetCollection(q, filterOptions)
+    // If the first filter option is truthy, add the query constraint
+    if (filterOptions.option1) {
+      queryConstraints.push(where("type_of_establishment", "==", filterOptions.option1))
+    }
+    if (filterOptions.option2) {
+    // If the second filter option is truthy, add the query constraint
+      queryConstraints.push(where("offers", "==", filterOptions.option2))
+    }
   }
-  
-  const q = query((collection(db, 'restaurants')))
+
+  const q = query(collection(db, 'restaurants'), ...queryConstraints)
   return useGetCollection(q, filterOptions)
 }
 
