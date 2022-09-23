9307435d71643314f992d390c5f8c23063dfb67d
olesien <olesien2@gmail.com>
2022-09-23 11:27:50 +0200

    Added sort options

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,15 +9,16 @@ import useGetRestaurants from "../hooks/useGetRestaurants";
 
 const HomePage = () => {
     const [userLocation, setUserLocation] = useState(false);
-    const [showFilter, setShowFilter] = useState(false)
-    const [filterOptions, setFilterOptions] = useState(null)
+    const [showFilter, setShowFilter] = useState(false);
+    const [sortBy, setSortBy] = useState(false);
+    const [filterOptions, setFilterOptions] = useState(null);
     const restaurants = useGetRestaurants(filterOptions);
 
-    console.log(filterOptions)
+    console.log(filterOptions);
 
     const handleSetFilterOptions = (options) => {
-        setFilterOptions(options)
-    }
+        setFilterOptions(options);
+    };
 
     useEffect(() => {
         let center = {
@@ -44,10 +45,33 @@ const HomePage = () => {
     return (
         <Container className="py-3">
             <h1>Welcome!</h1>
-            {userLocation ? <Map restaurants={restaurants} userLocation={userLocation} /> : <></>}
+            {userLocation ? (
+                <Map restaurants={restaurants} userLocation={userLocation} />
+            ) : (
+                <></>
+            )}
 
-            <Button className="mt-2" onClick={() => { setShowFilter(!showFilter) }}>Filter</Button>
-            {showFilter && (<FilterRestaurants handleSetFilterOptions={handleSetFilterOptions} />)}
+            <Button
+                className="mt-2"
+                onClick={() => {
+                    setSortBy(!sortBy);
+                }}
+            >
+                Sort By Name
+            </Button>
+            <Button
+                className="mt-2"
+                onClick={() => {
+                    setShowFilter(!showFilter);
+                }}
+            >
+                Filter
+            </Button>
+            {showFilter && (
+                <FilterRestaurants
+                    handleSetFilterOptions={handleSetFilterOptions}
+                />
+            )}
             <RestaurantList restaurants={restaurants} />
         </Container>
     );
