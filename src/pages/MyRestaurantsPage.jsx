72d3136cb8f9ee09c80c3e3b55be90d9320adcb9
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-09-24 21:20:26 +0200

    useGetRestaurant hook, updated boilerplates for MyRestaurantsPage and RestaurantPage

--- src/pages/MyRestaurantsPage.jsx
+++ src/pages/MyRestaurantsPage.jsx
@@ -1,13 +1,33 @@
-import { Button } from 'react-bootstrap'
-import Container from 'react-bootstrap/Container'
+import { Container, Button, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const MyRestaurantsPage = () => {
+	/**
+	 * @todo: Fråga Eric om useGetRestaurants
+	 */
+	const restaurants = [] 
 
 	return (
 		<Container className="py-3">
-			<h1 className="display-1">My Restaurants</h1>
-			<p>Map of restaurants</p>
-			<p>List of restaurants</p>
+			<h1>Your restaurants</h1>
+			<div className="list">
+				{restaurants &&
+					restaurants.length > 0 &&
+					restaurants.map((restaurant) => (
+						<Card style={{ width: "18rem", margin: "1rem" }} key={restaurant.id}>
+							<Card.Header>Distance</Card.Header>
+							<Card.Img variant="top" src={restaurant.url} />
+							<Card.Body>
+								<Card.Title>{restaurant.name}</Card.Title>
+								<Card.Subtitle className="mb-2 text-muted">{restaurant.cuisine} {restaurant.category}</Card.Subtitle>
+								<Card.Text>{restaurant.address}, {restaurant.postcode} {restaurant.city}</Card.Text>
+								<Card.Text><Card.Link>{restaurant.website}</Card.Link></Card.Text>
+								<Card.Text><Card.Link>{restaurant.tel}</Card.Link></Card.Text>
+								<Card.Text><Button variant='primary' as={Link} to={`/my-restaurants/${restaurant.id}`}>Read more</Button></Card.Text>
+							</Card.Body>
+						</Card>
+					))}
+			</div>
 			<Button>Add a restaurant</Button>
 		</Container>
 	)
--- src/pages/RestaurantPage.jsx
+++ src/pages/RestaurantPage.jsx
@@ -1,45 +1,66 @@
 import React from 'react'
 import { Button, Container } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+import useGetRestaurant from '../hooks/useGetRestaurant'
 
 const RestaurantPage = () => {
+    const { id } = useParams()
+    const { data: restaurant } = useGetRestaurant(id)
+
     return (
         <Container className="py-3">
-            <h1>Some restaurant name</h1>
-            <h3>Address</h3>
-            <p>615 Magnolia Blvd</p>
-            <h3>City</h3>
-            <p>91501, Burbank, CA</p>
-            <h3>Description</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa corporis, pariatur adipisci rem voluptatum velit repellat dignissimos iste debitis minus.</p>
-            <h3>Cuisine</h3>
-            <ul>
-                <li>Greek</li>
-                <li>Mediterranean</li>
-            </ul>
-            <h3>Restaurant type</h3>
-            <ul>
-                <li>Bar</li>
-                <li>Restaurant</li>
-            </ul>
-            <p>Offer</p>
-            <ul>
-                <li>Á la carte</li>
-                <li>After Work</li>
-            </ul>
-            <h2>Contact details</h2>
-            <h3>Email</h3>
-            <a href="mailto:info@email.com">Some link</a>
-            <h3>Phone</h3>
-            <a href="tel:+46123456789">Some link</a>
-            <h3>Website</h3>
-            <a href="https://google.com" target="_blank">Some link</a>
-            <h3>Facebook</h3>
-            <a href="https://facebook.com" target="_blank">Some link</a>
-            <h3>Instagram</h3>
-            <a href="https://instagram.com" target="_blank">Some link</a>
-            <p>
-                <Button>Upload photo</Button>
-            </p>
+
+            {!restaurant && (
+                 <h1>Sorry, that restaurant could not be found 😔</h1>
+            )}
+
+            {restaurant && (
+                <>
+                    <h1>{restaurant.name}</h1>
+                    <h3>Address</h3>
+                    <p>{restaurant.address}, {restaurant.postcode} {restaurant.city}</p>
+                    <h3>Description</h3>
+                    <p>{restaurant.description}</p>
+                    <h3>Cuisine</h3>
+                    <ul>
+                        {restaurant.cuisine &&
+                            restaurant.cuisine.map((cuisine, index) => (
+                                <li key={index}>{cuisine}</li>
+                            ))
+                        }
+                    </ul>
+                    <h3>Restaurant type</h3>
+                    <ul>
+                        {restaurant.category &&
+                            restaurant.category.map((category, index) => (
+                                <li key={index}>{category}</li>
+                            ))
+                        }
+                    </ul>
+                    <p>Offer</p>
+                    <ul>
+                        {restaurant.offer &&
+                            restaurant.offer.map((offer, index) => (
+                                <li key={index}>{offer}</li>
+                            ))
+                        }
+                    </ul>
+                    <h2>Contact details</h2>
+                    <h3>Email</h3>
+                    <a href={`mailto:${restaurant.email}`}>{restaurant.email}</a>
+                    <h3>Phone</h3>
+                    <a href={`tel:${restaurant.tel}`}>{restaurant.tel}</a>
+                    <h3>Website</h3>
+                    <a href={`https://www.${restaurant.website}`}>{restaurant.website}</a>
+                    <h3>Facebook</h3>
+                    <a href={`https://www.${restaurant.facebook}`}>{restaurant.facebook}</a>
+                    <h3>Instagram</h3>
+                    <a href={`https://www.${restaurant.instagram}`}>{restaurant.instagram}</a>
+                    <p>
+                        <Button>Upload photo</Button>
+                    </p>
+                </>
+            )}
         </Container>
     )
 }
