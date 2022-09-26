dbb8d60b2765037c86e08c80e880b46d46769502
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-09-26 19:09:41 +0200

    loading state

--- src/hooks/useGetDocument.js
+++ src/hooks/useGetDocument.js
@@ -1,28 +1,33 @@
 import { doc, getDoc } from "firebase/firestore"
-import { useEffect } from "react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { db } from "../firebase"
 
 const useGetDocument = (collection, id) => {
     const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const getDocSnapshot = async () => {
+            setLoading(true)
             // get reference to document in collection 'restaurants'
             const ref = doc(db, collection, id)
             const snapshot = await getDoc(ref)
 
-            if(snapshot.exists()) {
-                setData(snapshot.data())
-            } else {
+            if (!snapshot.exists()) {
                 setData(false)
+                setLoading(false)
+                return
             }
+
+            setData(snapshot.data())
+            setLoading(false)
         }
         getDocSnapshot()
     }, [])
 
     return {
         data,
+        loading,
     }
 }
 
