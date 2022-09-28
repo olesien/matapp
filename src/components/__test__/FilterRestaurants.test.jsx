8c1f78ea80dce95c2a1a949404ec1c1de0d15217
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2022-09-28 15:49:29 +0200

    Now testing if the filter form has the corresponding values from the input fields.

--- src/components/__test__/FilterRestaurants.test.jsx
+++ src/components/__test__/FilterRestaurants.test.jsx
@@ -13,9 +13,28 @@ test("The current values of the dropdown forms can be changed", async () => {
     // interact
     await userEvent.selectOptions(typeDropdown, ["restaurant"])
     await userEvent.selectOptions(offeringDropdown, ["lunch"])
-    
+
     // assert
-    
-    expect(screen.getByRole('option', {name: /restaurant/i}).selected).toBe(true)
-    expect(screen.getByRole('option', {name: /lunch/i}).selected).toBe(true)
+
+    expect(screen.getByRole('option', { name: /restaurant/i }).selected).toBe(true)
+    expect(screen.getByRole('option', { name: /lunch/i }).selected).toBe(true)
+})
+
+test("The form has the corresponding values from the input fields", async () => {
+    // render
+    render(<FilterRestaurants />)
+
+    // find
+    const typeDropdown = screen.getByLabelText(/type of restaurant/i)
+    const offeringDropdown = screen.getByLabelText(/serving/i)
+
+    // interact
+    await userEvent.selectOptions(typeDropdown, ["restaurant"])
+    await userEvent.selectOptions(offeringDropdown, ["lunch"])
+
+    // assert
+    expect(screen.getByRole('form')).toHaveFormValues({
+        "type-select": "restaurant",
+        "serving-select": "lunch"
+    })
 })
