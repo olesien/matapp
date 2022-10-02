7770c372061570048b070035f287f07765168310
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-10-02 12:14:58 +0200

    RestaurantPage update

--- src/pages/RestaurantPage.jsx
+++ src/pages/RestaurantPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Container } from 'react-bootstrap'
+import { Button, Container, Row, Col, Card, Image } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import useGetRestaurant from '../hooks/useGetRestaurant'
 
@@ -8,7 +8,7 @@ const RestaurantPage = () => {
     const { data: restaurant, loading } = useGetRestaurant(id)
 
     return (
-        <Container className="py-3">
+        <Container className="py-3 center-y">
 
             {loading && <p>Loading...</p>}
 
@@ -17,33 +17,45 @@ const RestaurantPage = () => {
             )}
 
             {!loading && restaurant && (
-                <>
-                    <h1>{restaurant.name}</h1>
-                    <h3>Address</h3>
-                    <p>{restaurant.address}, {restaurant.postcode} {restaurant.city}</p>
-                    <h3>Description</h3>
-                    <p>{restaurant.description}</p>
-                    <h3>Cuisine</h3>
-                    <p>{restaurant.cuisine}</p>
-                    <h3>Category</h3>
-                    <p>{restaurant.category}</p>
-                    <h3>Offer</h3>
-                    <p>{restaurant.offer}</p>
-                    <h2>Contact details</h2>
-                    <h3>Email</h3>
-                    <a href={`mailto:${restaurant.email}`}>{restaurant.email}</a>
-                    <h3>Phone</h3>
-                    <a href={`tel:${restaurant.tel}`}>{restaurant.tel}</a>
-                    <h3>Website</h3>
-                    <a href={`https://www.${restaurant.website}`}>{restaurant.website}</a>
-                    <h3>Facebook</h3>
-                    <a href={`https://www.${restaurant.facebook}`}>{restaurant.facebook}</a>
-                    <h3>Instagram</h3>
-                    <a href={`https://www.${restaurant.instagram}`}>{restaurant.instagram}</a>
-                    <p>
-                        <Button>Upload photo</Button>
-                    </p>
-                </>
+                <Row>
+                    <Col md={{ span: 6, offset: 3 }}>
+                        <Card>
+                            <Card.Header as="h5">{restaurant.name}</Card.Header>
+                            <Card.Body>
+                                <div className="d-flex justify-content-center my-3">
+                                    <Image
+                                        src='https://via.placeholder.com/225'
+                                        fluid
+                                    />
+                                </div>
+                                <h5>Address</h5>
+                                <p>{restaurant.address}, {restaurant.postcode} {restaurant.city}</p>
+                                <h5>Description</h5>
+                                <p>{restaurant.description}</p>
+                                <h5>Cuisine</h5>
+                                <p>{restaurant.cuisine}</p>
+                                <h5>Category</h5>
+                                <p>{restaurant.category}</p>
+                                <h5>Offer</h5>
+                                <p>{restaurant.offer}</p>
+                                <h4>Contact details</h4>
+                                <h5>Email</h5>
+                                <a href={`mailto:${restaurant.email}`}>{restaurant.email}</a>
+                                <h5>Phone</h5>
+                                <a href={`tel:${restaurant.tel}`}>{restaurant.tel}</a>
+                                <h5>Website</h5>
+                                <a href={`https://www.${restaurant.website}`}>{restaurant.website}</a>
+                                <h5>Facebook</h5>
+                                <a href={`https://www.${restaurant.facebook}`}>{restaurant.facebook}</a>
+                                <h5>Instagram</h5>
+                                <a href={`https://www.${restaurant.instagram}`}>{restaurant.instagram}</a>
+                                <div className='mt-3'>
+                                    <Button>Upload photo</Button>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
             )}
         </Container>
     )
