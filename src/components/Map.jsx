f52ec41142d415f6c1f385502e7cad092f40c8db
olesien <olesien2@gmail.com>
2022-10-05 15:35:42 +0200

    Set map type control to false
    To hide satellite view :)

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,18 +3,12 @@ import {
     GoogleMap,
     useJsApiLoader,
     Marker,
-    InfoWindow,
-    InfoBox,
     StandaloneSearchBox,
     OverlayView,
-    Circle,
 } from "@react-google-maps/api";
 import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import useGetRestaurants from "../hooks/useGetRestaurants";
 import MyLocation from "./MyLocation";
-import { useQuery } from "react-query";
-import GeocodingAPI from "../services/GeocodingAPI";
 import { useSearchParams } from "react-router-dom";
 
 const libraries = ["places"];
@@ -25,45 +19,8 @@ const containerStyle = {
 };
 
 const defaultZoom = 10;
-//const position = { lat: 33.872, lng: -117.214 };
-
-// const positions = [
-//     {
-//         lat: 33.772,
-//         lng: -117.214,
-//         title: "Small Shanghai",
-//         url: "https://www.china-briefing.com/news/wp-content/uploads/2019/04/China-Briefing-Shanghai-Industry-Economics-and-Policy.jpg",
-//     },
-//     {
-//         lat: 33.672,
-//         lng: -117.214,
-//         title: "Japanese samurai special",
-//         url: "https://cdn.britannica.com/71/196871-050-B8665B11/Samurai-Armour-Kusakabe-Kimbei.jpg",
-//     },
-//     {
-//         lat: 33.572,
-//         lng: -117.214,
-//         title: "Swedish meatballs",
-//         url: "https://www.recipetineats.com/wp-content/uploads/2017/01/Swedish-Meatballs_2-SQ.jpg",
-//     },
-// ];
-
-//const userLocation = { lat: 55.612, lng: 13.011 };
-//const userLocation = { lat: 33.872, lng: -117.214 };
-//const userLocation = { lat: 55.872, lng: -13.214 };
 
 const Map = ({ restaurants, userLocation, handleSetMapReference }) => {
-    // const location = useQuery(
-    //     ["location", 40.714224, -73.961452],
-    //     GeocodingAPI.getReverseGeocode
-    // );
-    // const latlng = useQuery(
-    //     ["latlng", "24%20Sussex%20Drive%20Ottawa%20ON"],
-    //     GeocodingAPI.getGeocode
-    // );
-    // console.log(location);
-    // console.log(latlng);
-    // console.log(userLocation);
     const [restaurant, setRestaurant] = useState(null);
 
     const [currentZoom, setCurrentZoom] = useState(defaultZoom);
@@ -76,31 +33,22 @@ const Map = ({ restaurants, userLocation, handleSetMapReference }) => {
     const [searchParams, setSearchParams] = useSearchParams();
 
     const [currentLocation, setCurrentLocation] = useState(userLocation);
-    //const [center, setCenter] = useState(userLocation);
-    // const restaurants = useGetRestaurants();
-    //console.log(restaurants);
     const [map, setMap] = React.useState(null);
-    //console.log(map);
-
     const divStyle = {
         background: `white`,
         border: `1px solid #ccc`,
         padding: 15,
     };
 
-    const mapContainerStyle = {};
-
     const handleCenterChanged = () => {
         if (map) {
             const newCenter = map.getCenter();
-            //console.log(newCenter.lat(), newCenter.lng());
             setCurrentLocation({ lat: newCenter.lat(), lng: newCenter.lng() });
         }
     };
     const handleZoomChanged = () => {
         if (map) {
             const newZoom = map.getZoom();
-            //console.log(newZoom);
             setCurrentZoom(newZoom);
         }
     };
@@ -118,10 +66,6 @@ const Map = ({ restaurants, userLocation, handleSetMapReference }) => {
     const onUnmount = React.useCallback(function callback(map) {
         setMap(null);
     }, []);
-
-    const options = { closeBoxURL: "", enableEventPropagation: true };
-
-    // Search functionality within map
 
     // Gain access to the searchBox property from Search Box component
     const [searchBox, setSearchBox] = useState(null);
@@ -165,6 +109,7 @@ const Map = ({ restaurants, userLocation, handleSetMapReference }) => {
                 ],
             },
         ],
+        mapTypeControl: false,
     };
 
     return isLoaded ? (
