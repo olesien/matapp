337a29484eda1c2862f9ff1a7a5d3bf138d04ca9
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-10-05 20:15:34 +0200

    show different toast message if admin or not

--- src/components/CreateRestaurantForm.jsx
+++ src/components/CreateRestaurantForm.jsx
@@ -83,7 +83,11 @@ const CreateRestaurantForm = () => {
 
             // url: "https://firebasestorage.googleapis.com/v0/b/fed21-matguiden.appspot.com/o/restaurants%2F1663942025-london-stock.jpg?alt=media&token=bc832727-0b00-41a2-ac98-4425bbd87102",
 
-            toast.success("Restaurant sent for admin approval");
+            if (currentUser.admin) {
+                toast.success("Restaurant successfully created");
+            } else {
+                toast.success("Restaurant sent for admin approval");
+            }
             reset();
         }
     };
